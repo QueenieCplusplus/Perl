92ce75105cc7d4463e52873f2d00a9c8b71c5d62
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2019-12-28 12:14:41 +0800

    接受字串運算式作為參數

---
+++ eval.pl
@@ -0,0 +1,16 @@
+//eval 
+//2019, 12/28, by Queenie Chen
+
+//除了偵測錯誤外
+//evel 還能接受字串運算式作為參數
+//直到執行階段才編譯和執行字串內容
+//雖然好用但很危險
+//當字串內有不可靠資料的時候
+
+
+eval '$sum = 2 + 2';
+print " The sum is $sum\n";
+
+
+q $result = eval "2+2";
+print "the result is $result\n"
